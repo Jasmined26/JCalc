b1d5aab435d0d2c337ab8ce6f7362cc31940e076
Jasmine Jenkins <jasminedjenkins26@gmail.com>
2021-01-05 01:01:28 -0500

    Upload JCalc.py

---
+++ JCalc.py
@@ -0,0 +1,15 @@
+# Import modules
+import tkinter
+import tkinter.font as tkFont
+from tkinter import *
+
+# GUI class
+class standardCalculator:
+    def __init__(self):
+        # Create main window
+        self.main_window = tkinter.Tk()
+
+        tkinter.mainloop()
+
+
+calc1 = standardCalculator()
